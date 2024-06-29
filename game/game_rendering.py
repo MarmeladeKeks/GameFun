60685f6edb6280e3425dc6f64d6e23a371a5a428
Ingo <ingo.puerner@fau.de>
2024-06-29 17:22:04 +0200

    WIP first draft of working "player" movement

--- game/game_rendering.py
+++ game/game_rendering.py
@@ -1,14 +1,24 @@
 import pygame
+import numpy as np
 
 
 BLUE = (0, 0, 220)
+ROT = (200, 0, 0)
+WORLD_GRAV = 0.0005  # (m / s^2)
 
 
 class GameRendering:
     def __init__(self):
-        self.render()
         self.screen = None
-        self.clock = None
+        self.clock: pygame.time.Clock = None
+        self.rect: np.ndarray = np.array([0, 0, 100, 100, 1], dtype=float)
+        self.player: np.ndarray = np.array([0, 320, 20, 20], dtype=float)
+        self.time = 0
+        self.vel: np.ndarray = np.array([0, 0], dtype=float)
+        self.acceleration = WORLD_GRAV
+        self.jump_cooldown_timer = 0
+
+        self.render()
 
     def render(self):
         pygame.init()
@@ -20,16 +30,19 @@ class GameRendering:
 
         # Main Execution Loop
         while game_active:
+            self.time = self.clock.get_time()
+            self.acceleration = WORLD_GRAV
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     game_active = False
                 elif event.type == pygame.KEYDOWN:
                     self.handle_key_events(event)
 
-            # Here Game Logic
 
-            # Spielfeld/figur(en) zeichnen (davor Spielfeld löschen)
+            # Spielfeld/figur(en) zeichnen (davor Spielfeld löschen) and Game Logic
             self.screen.fill(BLUE)  # clear screen
+            self.player_motion()
 
             # Fenster aktualisieren
             pygame.display.flip()
@@ -40,6 +53,31 @@ class GameRendering:
         pygame.quit()
 
     def handle_key_events(self, event: pygame.event.Event):
-        print("Player pressed a key")
         if event.key == pygame.K_SPACE:
-            print("Spieler hat Leertaste gedrückt")
+            self.acceleration -= 0.1  # setze Beschleunigung nach oben
+            self.jump_cooldown_timer = self.time
+            print("Space pressed")
+
+    def draw_rectangle(self):
+        pos_rect = self.rect[:2]
+        if pos_rect[0] + self.rect[2] > pygame.display.get_window_size()[0]:
+            self.rect[4] = -1
+        elif pos_rect[0] <= 0:
+            self.rect[4] = 1
+
+        self.rect[:2] = self.rect[:2] + (([80, 0] * self.time) * self.rect[4])
+        pygame.draw.rect(self.screen, ROT, rect=self.rect[:4])
+
+    def player_motion(self):
+        self.vel[1] += self.acceleration * self.time
+        player_y = self.player[1] + self.vel[1] * self.time
+
+        if player_y <= 0:
+            player_y = 0
+            self.vel[1] = 0
+        elif player_y + self.player[3] >= pygame.display.get_window_size()[1]:
+            player_y = pygame.display.get_window_size()[1] - self.player[3]
+            self.vel[1] = 0
+        self.player[1] = player_y
+
+        pygame.draw.rect(self.screen, ROT, rect=self.player)
